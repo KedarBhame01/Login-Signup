1932d38047113f5e47d306caff2f0b46b60e856a
kedar <kedarbhame1999@gmail.com>
2025-07-16 18:01:10 +0530

    done basic api

--- LoginSignup/base/urls.py
+++ LoginSignup/base/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
-from .views import authView, home
+from .views import authView, home, StudentViewSet
 from django.contrib.auth import views as auth_views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'students',StudentViewSet)
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("signup/", authView, name="signup"),
-    path("", home, name="home"),
+    path("home/", home, name="home"),
+    path("", include(router.urls)),
     # path("login/", auth_views.LoginView.as_view(), name="login"),  # Add this line
 ]
