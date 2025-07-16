46c10f7a427b9e65df3614c23e386ffe22459eb9
KedarBhame01 <kedarbhame1999@gmail.com>
2025-07-16 06:41:51 +0530

    Add home view and template, update auth flow
    Introduced a new home view and template, protected by login. Updated URL routing to use namespaces and added named routes. Set LOGIN_REDIRECT_URL and LOGOUT_REDIRECT_URL in settings for improved authentication flow. Improved signup view logic and added home page route.

--- LoginSignup/base/urls.py
+++ LoginSignup/base/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path,include
-from .views import authView
+from django.urls import path, include
+from .views import authView, home
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("accounts/",include("django.contrib.auth.urls")),
-    path("signup/",authView)
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("signup/", authView, name="authView"),
+    path("", home, name="home"),
+    # path("login/", auth_views.LoginView.as_view(), name="login"),  # Add this line
 ]
