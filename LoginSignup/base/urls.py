ef4a4779077dc66c65795f2cf426b3cd099a2fc7
kedar <kedarbhame1999@gmail.com>
2025-07-17 12:53:51 +0530

    Add export to Excel and PDF for student list
    Added export buttons to the student list template and implemented views to export student data to Excel and PDF formats. Updated URLs to include routes for these new export functionalities.

--- LoginSignup/base/urls.py
+++ LoginSignup/base/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.student_form_view, name='student_form'),
     path('delete/<int:id>/', views.delete_student, name='delete_student'),
     path('edit/<int:id>/', views.edit_student, name='edit_student'),
+    path('export/excel/',views.export_students_excel, name='export_excel'),
+    path('export/pdf/',views.export_students_pdf, name='export_students_pdf'),
 ]
--- LoginSignup/base/views.py
+++ LoginSignup/base/views.py
@@ -1,6 +1,12 @@
+import openpyxl
+from django.utils.encoding import smart_str
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student
 from .forms import StudentForm
+from django.http import HttpResponse
+# for export pdf -
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
 
 # Home page: show form + all students
 def student_form_view(request):
@@ -38,3 +44,55 @@ def edit_student(request, id):
     return render(request, 'registration/edit_student.html', {
         'form': form
     })
+
+def export_students_excel(request):
+    students = Student.objects.all()
+    
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.title = 'Student'
+
+    sheet.append(['ID','Name','Age'])
+    
+    for student in students:
+        sheet.append([student.id, student.name, student.age])
+        
+    response = HttpResponse(
+        content_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+    response['Content-Disposition'] = 'attachment; filename=students_info.xlsx'
+    
+    workbook.save(response)
+    return response
+
+def export_students_pdf(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename=students_info.pdf'
+
+    p = canvas.Canvas(response,pagesize=A4)
+    width, height = A4
+
+    p.setFont("Helvetica-Bold", 16)
+    p.drawString(200, height -50, "Student List")
+
+    p.setFont("Helvetica-Bold",12)
+    p.drawString(50, height - 100, "ID")
+    p.drawString(100, height -100, "Name")
+    p.drawString(300, height - 100, "Age")
+
+    y = height - 120
+    p.setFont("Helvetica",12)
+    
+    students = Student.objects.all()
+    for student in students:
+        p.drawString(50, y, str(student.id))
+        p.drawString(100, y, student.name)
+        p.drawString(300, y, str(student.age))
+        y -= 20
+        if y < 50:
+            p.showPage()
+            y = height - 50
+            
+    p.showPage()
+    p.save()
+    return response
